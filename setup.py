397bc5b98a9125acbf97e0ca47076edecea6a249
Martin Christen Frølund Thomsen <mcft@cbs.dtu.dk>
2016-05-30 19:42:35 +0200

    add uninstall end-point to setup.py

--- setup.py
+++ setup.py
@@ -20,10 +20,14 @@ def setup_package():
 
 def install_shell_scripts():
     repo_dir = os.path.dirname(os.path.realpath(__file__))
-    src, dst = repo_dir +"/update", repo_dir +"/env/bin/update"
-    os.chmod(src, 484) #0744
-    if not os.path.exists(dst):
-        os.symlink(src, dst)
+    scripts = [
+        (repo_dir +"/update", repo_dir +"/env/bin/update"),
+        (repo_dir +"/uninstall", repo_dir +"/env/bin/uninstall")
+        ]
+    for src, dst in scripts:
+        os.chmod(src, 484) #0744
+        if not os.path.exists(dst):
+            os.symlink(src, dst)
 
 if __name__ == "__main__":
     setup_package()
