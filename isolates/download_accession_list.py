812ce8e904f261ea05263297f49b599e5cd62177
Martin Christen Frølund Thomsen <mcft@cbs.dtu.dk>
2016-05-30 19:53:43 +0200

    debug

--- isolates/download_accession_list.py
+++ isolates/download_accession_list.py
@@ -303,7 +303,7 @@ def ProcessExperimentCombined(experiment_id, json, batch_dir, sample_dir_id, pre
                               experiment_id)
                 failed_accession.append(experiment_id)
     else:
-        _logger.error("Metadata Invalid! (%s) - %s", experiment_id, dir(m.metadata.items())
+        _logger.error("Metadata Invalid! (%s) - %s", experiment_id, m.metadata.items())
         failed_accession.append(experiment_id)
     return sample_dir_id
 
